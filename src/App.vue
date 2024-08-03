f432ac3f2c3a639089b49f36d54888e4d50918f3
13ruceYu <bruceyuhongbo@gmail.com>
2024-08-03 15:27:49 +0800

    git init

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import TextPoster from './components/TextPoster.vue'
+</script>
+
+<template>
+  <TextPoster></TextPoster>
+</template>
---
+++ src/components/TextPoster.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</template>
